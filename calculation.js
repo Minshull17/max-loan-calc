aad4804305a288e3884f067c28174af57502f459
Minshull17 <74798165+Minshull17@users.noreply.github.com>
2023-10-17 15:33:59 +0100

    Update calculation.js

--- calculation.js
+++ calculation.js
@@ -7,8 +7,12 @@ function calculateLoan() {
     var interestRate = parseFloat(document.getElementById('interestRate').value);
 
     // Perform calculations
-    var maxLoanAmount = propertyValue * (ltvPercentage / 100);
-    var minRentalIncome = propertyValue * (rentalCoverageRatio / 100);
+    var maxLoanAmountLTV = propertyValue * (ltvPercentage / 100);
+    var maxLoanAmountRental = propertyValue * (rentalCoverageRatio / 100);
+    
+    // Calculate estimated monthly payment using the lower of the two max loan amounts
+    var maxLoanAmount = Math.min(maxLoanAmountLTV, maxLoanAmountRental);
+    
     var monthlyInterest = (interestRate / 100) / 12;
     var loanTermMonths = 30 * 12; // Assuming a 30-year mortgage
 
@@ -17,8 +21,9 @@ function calculateLoan() {
     // Display the result
     var resultElement = document.getElementById('result');
     resultElement.innerHTML = `
-        <p>Maximum Loan Amount: $${maxLoanAmount.toFixed(2)}</p>
-        <p>Minimum Rental Income Required: $${minRentalIncome.toFixed(2)}</p>
+        <p>Maximum Loan Amount (LTV): $${maxLoanAmountLTV.toFixed(2)}</p>
+        <p>Maximum Loan Amount (Rental Coverage): $${maxLoanAmountRental.toFixed(2)}</p>
         <p>Estimated Monthly Payment: $${monthlyPayment.toFixed(2)}</p>
+        <p>Maximum Loan Amount (Lower of LTV and Rental Coverage): $${maxLoanAmount.toFixed(2)}</p>
     `;
 }
